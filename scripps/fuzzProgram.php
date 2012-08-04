4eff95bd108fcedf9abfae9e25f0067d66a73ae0
Lingam Ravi Kiran <l_ravikiran_2000@yahoo.com>
2012-08-04 10:57:00 +0530

    Small changes in fuzz programs

--- scripps/fuzzProgram.php
+++ scripps/fuzzProgram.php
@@ -9,7 +9,17 @@ for($i = 1; $i < 102; $i++) {
 }
 
 foreach($numList as $key => $value) {
-	echo $key ." -- ". $value."\n";
+	$threeVal = $value%3;
+	$fiveVal = $value%5;
+	if ($threeVal == 0 && $fiveVal == 0) {
+		echo "FuzzBuzz\n";
+	} else if ($threeVal != 0 && $fiveVal == 0) {
+		echo "Buzz\n";
+	} else if ($threeVal == 0 && $fiveVal != 0) {
+		echo "Fuzz\n";
+	} else {
+		echo $value."\n";
+	}
 }
 
 ?>
